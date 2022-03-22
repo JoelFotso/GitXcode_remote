55dbc6e6eaf852b91d6d2eebd2554417147e3ce2
Joel Fotso Simo <joelf.simo@gmail.com>
2022-03-22 12:43:47 +0100

    Initial Commit

---
+++ GitXcode/GitXcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitXcodeApp.swift
+//  GitXcode
+//
+//  Created by FsJOEL on 22.03.22.
+//
+
+import SwiftUI
+
+@main
+struct GitXcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
